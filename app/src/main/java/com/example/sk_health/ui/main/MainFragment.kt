bbdaf6e4ed1188a140d8f5597ae6dc7a5044fba4
Taras Havryliak <72496644+THavryliak@users.noreply.github.com>
2022-04-14 01:58:35 +0300

    'init'

---
+++ app/src/main/java/com/example/sk_health/ui/main/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.sk_health.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sk_health.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/com/example/sk_health/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sk_health.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
